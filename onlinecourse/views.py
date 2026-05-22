a641837e2a501b544d60286719cb568ab34b134a
fundimi <ukdimilifegains@gmail.com>
2026-05-22 00:56:21 -0300

    Integrate Bootstrap into Django templates

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render, redirect
 from .models import Course, Lesson, Enrollment
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import Http404
 from django.urls import reverse
 from django.views import generic, View
-from django.http import Http404
-from django.contrib.auth import logout, authenticate, login
+from collections import defaultdict
+from django.contrib.auth import login, logout, authenticate
 import logging
 
 from django.contrib.auth.models import User
+
+# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 # Create your views here.
@@ -87,14 +91,17 @@ def logout_request(request):
 #         context['course_list'] = course_list
 #         return render(request, 'onlinecourse/course_list.html', context)
 
+# Add a class-based course list view
 class CourseListView(generic.ListView):
     template_name = 'onlinecourse/course_list.html'
     context_object_name = 'course_list'
 
     def get_queryset(self):
-        courses = Course.objects.order_by('-total_enrollment')[:10]
-        return courses
+       courses = Course.objects.order_by('-total_enrollment')[:10]
+       return courses
 
+
+# Add a generic course details view
 class CourseDetailsView(generic.DetailView):
     model = Course
     template_name = 'onlinecourse/course_detail.html'
@@ -115,17 +122,15 @@ class CourseDetailsView(generic.DetailView):
 #        )
 
 class EnrollView(View):
+
+    # Handles get request
     def post(self, request, *args, **kwargs):
         course_id = kwargs.get('pk')
         course = get_object_or_404(Course, pk=course_id)
-
+        # Create an enrollment
         course.total_enrollment += 1
         course.save()
-
-        return HttpResponseRedirect(
-            reverse(viewname='onlinecourse:course_details', args=(course.id,))
-        )
-
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
 
 
 # def course_details(request, course_id):
